4ca485706b07938133e40c9f594a7f9485328450
hann <hmweeks93@gmail.com>
2021-06-09 11:42:33 -0500

    added list componenets to Kennels.js

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -18,6 +18,25 @@ export const Kennel = () => (
             <AnimalProvider>
                 <AnimalList />
             </AnimalProvider>
+        </article>
+        <h2>Locations</h2>
+        <article className="locations">
+            <LocationProvider>
+                <LocationList />
+            </LocationProvider>
+        </article>
+        <h2>Employees</h2>
+        <article className="employees">
+            <EmployeeProvider>
+                <EmployeeList />
+            </EmployeeProvider>
+
+        </article>
+        <h2>Customers</h2>
+        <article className="customers">
+            <CustomerProvider>
+                <CustomerList />
+            </CustomerProvider>
 
         </article>
     </>
